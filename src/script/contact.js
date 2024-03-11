81b8e0058e91d82f079977b3a4f02ae6c177bfea
Zam <samuel.kodehode@gmail.com>
2024-03-11 15:18:16 +0100

    1.2.2

--- src/script/contact.js
+++ src/script/contact.js
@@ -1,62 +1,68 @@
 import { getRandomNumberInRange } from './script.js';
-const canvas2 = document.getElementById('contact-canvas');
-const ctx2 = canvas2.getContext('2d');
-const squareArrayLayer1 = [];
-const squareArrayLayer2 = [];
-const squareArrayLayer3 = [];
-const squareArrayLayer4 = [];
-const squareArrayLayer5 = [];
-canvas2.width = window.innerWidth;
-canvas2.height = window.innerHeight;
-const getRandomNumberInRange2 = (min, max) => Math.random() * (max - min) + min;
-const getSquares = (maxHeight, color, array, num) => {
-    for (let i = 0; i < num; i++) {
-        const random = getRandomNumberInRange(0, 1) === 1;
-        const square = {
-            x: (canvas2.width / num) * i,
-            y: canvas2.height,
-            vel: 0,
-            up: random,
-            h: 0,
-            w: 1,
-            maxHeightDivider: maxHeight,
-            color: color,
-            num: num
-        };
-        array.push(square);
-    }
-};
-const renderSquares = (array) => {
-    array.forEach((square) => {
-        if (square.h === 0)
-            square.h = -getRandomNumberInRange(canvas2.height / (square.maxHeightDivider + 2), canvas2.height / square.maxHeightDivider);
-        if (square.h < -canvas2.height / square.maxHeightDivider) {
-            square.up = false;
+function main() {
+    const canvas2 = document.getElementById('contact-canvas');
+    const ctx2 = canvas2.getContext('2d');
+    const squareArrayLayer1 = [];
+    const squareArrayLayer2 = [];
+    const squareArrayLayer3 = [];
+    const squareArrayLayer4 = [];
+    const squareArrayLayer5 = [];
+    canvas2.width = window.innerWidth;
+    canvas2.height = window.innerHeight;
+    const getRandomNumberInRange2 = (min, max) => Math.random() * (max - min) + min;
+    const getSquares = (maxHeight, color, array, num) => {
+        for (let i = 0; i < num; i++) {
+            const random = getRandomNumberInRange(0, 1) === 1;
+            const square = {
+                x: (canvas2.width / num) * i,
+                y: canvas2.height,
+                vel: 0,
+                up: random,
+                h: 0,
+                w: 1,
+                maxHeightDivider: maxHeight,
+                color: color,
+                num: num
+            };
+            array.push(square);
         }
-        else if (square.h > -canvas2.height / (square.maxHeightDivider + 1)) {
-            square.up = true;
-        }
-        if (square.up)
-            square.h += getRandomNumberInRange2(-0.2, -0.01);
-        if (!square.up)
-            square.h += getRandomNumberInRange2(0.01, 0.2);
-        ctx2.fillStyle = square.color;
-        ctx2.fillRect(square.x, square.y, canvas2.width / square.num + 2, square.h);
-    });
-};
-getSquares(8, 'rgb(229, 229, 229)', squareArrayLayer1, 20);
-getSquares(6, 'rgb(109,109,109)', squareArrayLayer2, 20);
-getSquares(5, 'rgb(62,60,60)', squareArrayLayer3, 20);
-getSquares(4, 'rgb(44,43,43)', squareArrayLayer4, 20);
-getSquares(3, 'rgb(19,19,19)', squareArrayLayer5, 20);
-const squareAnimationLoop = () => {
-    ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
-    renderSquares(squareArrayLayer5);
-    renderSquares(squareArrayLayer4);
-    renderSquares(squareArrayLayer3);
-    renderSquares(squareArrayLayer2);
-    renderSquares(squareArrayLayer1);
-    requestAnimationFrame(squareAnimationLoop);
-};
-squareAnimationLoop();
+    };
+    const renderSquares = (array) => {
+        array.forEach((square) => {
+            if (square.h === 0)
+                square.h = -getRandomNumberInRange(canvas2.height / (square.maxHeightDivider + 2), canvas2.height / square.maxHeightDivider);
+            if (square.h < -canvas2.height / square.maxHeightDivider) {
+                square.up = false;
+            }
+            else if (square.h > -canvas2.height / (square.maxHeightDivider + 1)) {
+                square.up = true;
+            }
+            if (square.up)
+                square.h += getRandomNumberInRange2(-0.2, -0.01);
+            if (!square.up)
+                square.h += getRandomNumberInRange2(0.01, 0.2);
+            ctx2.fillStyle = square.color;
+            ctx2.fillRect(square.x, square.y, canvas2.width / square.num + 2, square.h);
+        });
+    };
+    getSquares(8, 'rgb(229, 229, 229)', squareArrayLayer1, 20);
+    getSquares(6, 'rgb(109,109,109)', squareArrayLayer2, 20);
+    getSquares(5, 'rgb(62,60,60)', squareArrayLayer3, 20);
+    getSquares(4, 'rgb(44,43,43)', squareArrayLayer4, 20);
+    getSquares(3, 'rgb(19,19,19)', squareArrayLayer5, 20);
+    const squareAnimationLoop = () => {
+        ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
+        renderSquares(squareArrayLayer5);
+        renderSquares(squareArrayLayer4);
+        renderSquares(squareArrayLayer3);
+        renderSquares(squareArrayLayer2);
+        renderSquares(squareArrayLayer1);
+        requestAnimationFrame(squareAnimationLoop);
+    };
+    squareAnimationLoop();
+}
+main();
+window.addEventListener('resize', () => {
+    main();
+});
 //# sourceMappingURL=contact.js.map
